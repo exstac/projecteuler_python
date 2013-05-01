013622685e67d6c2cc0992ce123109a3d014c4f4
Oscar Vestlie <oscar@spotify.com>
2013-05-01 15:48:13 +0200

    moved sigma function to utils, cleaned up utils some

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,16 +1,23 @@
 from primes import gen_sieve
+from math import sqrt
 
-g = gen_sieve()
-primes = [g.next()]
 
 def tau(n):
+    g = gen_sieve()
+    primes = [g.next()]
     x = n
     d, i = 1, 0
     while x != 1:
         a = 0
         while x % primes[i] == 0 and x != 1:
             x, a = x/primes[i], a+1
-        if a > 0: d *= a+1
+        if a > 0:
+            d *= a + 1
         i += 1
-        if i+1 > len(primes): primes.append(g.next())
+        if i + 1 > len(primes):
+            primes.append(g.next())
     return d
+
+
+def sig(n):
+    return sum(i + n/i for i in range(1, int(sqrt(n)) + 1) if n % i == 0) - n
--- prob021.py
+++ prob021.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-from math import sqrt
-
-
-def sig(n):
-    return sum(i + n/i for i in range(1, int(sqrt(n)) + 1) if n % i == 0) - n
+from helpers.utils import sig
 
 print sum(i for i in range(1, 10000) if sig(sig(i)) == i and i != sig(i))
